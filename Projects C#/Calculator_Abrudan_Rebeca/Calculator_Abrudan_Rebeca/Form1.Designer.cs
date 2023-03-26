b406f827dd30b7452c7c401e8e60b89fe9085068
rrebi <97788460+rrebi@users.noreply.github.com>
2023-03-26 15:57:19 +0300

    calculator done

---
+++ Projects C#/Calculator_Abrudan_Rebeca/Calculator_Abrudan_Rebeca/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace Calculator_Abrudan_Rebeca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.b_1 = new System.Windows.Forms.Button();
+            this.b_2 = new System.Windows.Forms.Button();
+            this.b_3 = new System.Windows.Forms.Button();
+            this.b_6 = new System.Windows.Forms.Button();
+            this.b_5 = new System.Windows.Forms.Button();
+            this.b_4 = new System.Windows.Forms.Button();
+            this.b_9 = new System.Windows.Forms.Button();
+            this.b_8 = new System.Windows.Forms.Button();
+            this.b_7 = new System.Windows.Forms.Button();
+            this.b_0 = new System.Windows.Forms.Button();
+            this.b_c = new System.Windows.Forms.Button();
+            this.b_Sterge = new System.Windows.Forms.Button();
+            this.b_Plus = new System.Windows.Forms.Button();
+            this.b_Imp = new System.Windows.Forms.Button();
+            this.b_Ori = new System.Windows.Forms.Button();
+            this.b_Minus = new System.Windows.Forms.Button();
+            this.b_Putere = new System.Windows.Forms.Button();
+            this.b_Egal = new System.Windows.Forms.Button();
+            this.b_Rad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Honeydew;
+            this.label1.Location = new System.Drawing.Point(34, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 60);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(34, 433);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "(c) 2020, Abrudan Rebeca";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // b_1
+            // 
+            this.b_1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_1.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_1.Location = new System.Drawing.Point(37, 164);
+            this.b_1.Name = "b_1";
+            this.b_1.Size = new System.Drawing.Size(60, 60);
+            this.b_1.TabIndex = 2;
+            this.b_1.Text = "1";
+            this.b_1.UseVisualStyleBackColor = false;
+            this.b_1.Click += new System.EventHandler(this.b_1_Click);
+            // 
+            // b_2
+            // 
+            this.b_2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_2.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_2.Location = new System.Drawing.Point(103, 164);
+            this.b_2.Name = "b_2";
+            this.b_2.Size = new System.Drawing.Size(60, 60);
+            this.b_2.TabIndex = 3;
+            this.b_2.Text = "2";
+            this.b_2.UseVisualStyleBackColor = false;
+            this.b_2.Click += new System.EventHandler(this.b_2_Click);
+            // 
+            // b_3
+            // 
+            this.b_3.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_3.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_3.Location = new System.Drawing.Point(169, 164);
+            this.b_3.Name = "b_3";
+            this.b_3.Size = new System.Drawing.Size(60, 60);
+            this.b_3.TabIndex = 4;
+            this.b_3.Text = "3";
+            this.b_3.UseVisualStyleBackColor = false;
+            this.b_3.Click += new System.EventHandler(this.b_3_Click);
+            // 
+            // b_6
+            // 
+            this.b_6.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_6.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_6.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_6.Location = new System.Drawing.Point(169, 228);
+            this.b_6.Name = "b_6";
+            this.b_6.Size = new System.Drawing.Size(60, 60);
+            this.b_6.TabIndex = 7;
+            this.b_6.Text = "6";
+            this.b_6.UseVisualStyleBackColor = false;
+            this.b_6.Click += new System.EventHandler(this.b_6_Click);
+            // 
+            // b_5
+            // 
+            this.b_5.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_5.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_5.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_5.Location = new System.Drawing.Point(103, 228);
+            this.b_5.Name = "b_5";
+            this.b_5.Size = new System.Drawing.Size(60, 60);
+            this.b_5.TabIndex = 6;
+            this.b_5.Text = "5";
+            this.b_5.UseVisualStyleBackColor = false;
+            this.b_5.Click += new System.EventHandler(this.b_5_Click);
+            // 
+            // b_4
+            // 
+            this.b_4.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_4.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_4.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_4.Location = new System.Drawing.Point(37, 228);
+            this.b_4.Name = "b_4";
+            this.b_4.Size = new System.Drawing.Size(60, 60);
+            this.b_4.TabIndex = 5;
+            this.b_4.Text = "4";
+            this.b_4.UseVisualStyleBackColor = false;
+            this.b_4.Click += new System.EventHandler(this.b_4_Click);
+            // 
+            // b_9
+            // 
+            this.b_9.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_9.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_9.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_9.Location = new System.Drawing.Point(169, 294);
+            this.b_9.Name = "b_9";
+            this.b_9.Size = new System.Drawing.Size(60, 60);
+            this.b_9.TabIndex = 10;
+            this.b_9.Text = "9";
+            this.b_9.UseVisualStyleBackColor = false;
+            this.b_9.Click += new System.EventHandler(this.b_9_Click);
+            // 
+            // b_8
+            // 
+            this.b_8.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_8.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_8.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_8.Location = new System.Drawing.Point(103, 294);
+            this.b_8.Name = "b_8";
+            this.b_8.Size = new System.Drawing.Size(60, 60);
+            this.b_8.TabIndex = 9;
+            this.b_8.Text = "8";
+            this.b_8.UseVisualStyleBackColor = false;
+            this.b_8.Click += new System.EventHandler(this.b_8_Click);
+            // 
+            // b_7
+            // 
+            this.b_7.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_7.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_7.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_7.Location = new System.Drawing.Point(37, 294);
+            this.b_7.Name = "b_7";
+            this.b_7.Size = new System.Drawing.Size(60, 60);
+            this.b_7.TabIndex = 8;
+            this.b_7.Text = "7";
+            this.b_7.UseVisualStyleBackColor = false;
+            this.b_7.Click += new System.EventHandler(this.b_7_Click);
+            // 
+            // b_0
+            // 
+            this.b_0.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.b_0.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_0.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.b_0.Location = new System.Drawing.Point(39, 360);
+            this.b_0.Name = "b_0";
+            this.b_0.Size = new System.Drawing.Size(60, 60);
+            this.b_0.TabIndex = 11;
+            this.b_0.Text = "0";
+            this.b_0.UseVisualStyleBackColor = false;
+            this.b_0.Click += new System.EventHandler(this.b_0_Click);
+            // 
+            // b_c
+            // 
+            this.b_c.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(122)))), ((int)(((byte)(102)))));
+            this.b_c.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_c.ForeColor = System.Drawing.Color.Honeydew;
+            this.b_c.Location = new System.Drawing.Point(37, 98);
+            this.b_c.Name = "b_c";
+            this.b_c.Size = new System.Drawing.Size(60, 60);
+            this.b_c.TabIndex = 12;
+            this.b_c.Text = "c";
+            this.b_c.UseVisualStyleBackColor = false;
+            this.b_c.Click += new System.EventHandler(this.b_c_Click);
+            // 
+            // b_Sterge
+            // 
+            this.b_Sterge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(122)))), ((int)(((byte)(102)))));
+            this.b_Sterge.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_Sterge.ForeColor = System.Drawing.Color.Honeydew;
+            this.b_Sterge.Location = new System.Drawing.Point(103, 98);
+            this.b_Sterge.Name = "b_Sterge";
+            this.b_Sterge.Size = new System.Drawing.Size(60, 60);
+            this.b_Sterge.TabIndex = 13;
+            this.b_Sterge.Text = "<";
+            this.b_Sterge.UseVisualStyleBackColor = false;
+            this.b_Sterge.Click += new System.EventHandler(this.b_Sterge_Click);
+            // 
+            // b_Plus
+            // 
+            this.b_Plus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(143)))), ((int)(((byte)(112)))));
+            this.b_Plus.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_Plus.ForeColor = System.Drawing.Color.Honeydew;
+            this.b_Plus.Location = new System.Drawing.Point(254, 98);
+            this.b_Plus.Name = "b_Plus";
+            this.b_Plus.Size = new System.Drawing.Size(60, 60);
+            this.b_Plus.TabIndex = 14;
+            this.b_Plus.Text = "+";
+            this.b_Plus.UseVisualStyleBackColor = false;
+            this.b_Plus.Click += new System.EventHandler(this.b_Plus_Click);
+            // 
+            // b_Imp
+            // 
+            this.b_Imp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(143)))), ((int)(((byte)(112)))));
+            this.b_Imp.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_Imp.ForeColor = System.Drawing.Color.Honeydew;
+            this.b_Imp.Location = new System.Drawing.Point(254, 294);
+            this.b_Imp.Name = "b_Imp";
+            this.b_Imp.Size = new System.Drawing.Size(60, 60);
+            this.b_Imp.TabIndex = 15;
+            this.b_Imp.Text = "/";
+            this.b_Imp.UseVisualStyleBackColor = false;
+            this.b_Imp.Click += new System.EventHandler(this.b_Imp_Click);
+            // 
+            // b_Ori
+            // 
+            this.b_Ori.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(143)))), ((int)(((byte)(112)))));
+            this.b_Ori.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_Ori.ForeColor = System.Drawing.Color.Honeydew;
+            this.b_Ori.Location = new System.Drawing.Point(254, 228);
+            this.b_Ori.Name = "b_Ori";
+            this.b_Ori.Size = new System.Drawing.Size(60, 60);
+            this.b_Ori.TabIndex = 16;
+            this.b_Ori.Text = "*";
+            this.b_Ori.UseVisualStyleBackColor = false;
+            this.b_Ori.Click += new System.EventHandler(this.b_Ori_Click);
+            // 
+            // b_Minus
+            // 
+            this.b_Minus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(143)))), ((int)(((byte)(112)))));
+            this.b_Minus.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_Minus.ForeColor = System.Drawing.Color.Honeydew;
+            this.b_Minus.Location = new System.Drawing.Point(254, 164);
+            this.b_Minus.Name = "b_Minus";
+            this.b_Minus.Size = new System.Drawing.Size(60, 60);
+            this.b_Minus.TabIndex = 17;
+            this.b_Minus.Text = "-";
+            this.b_Minus.UseVisualStyleBackColor = false;
+            this.b_Minus.Click += new System.EventHandler(this.b_Minus_Click);
+            // 
+            // b_Putere
+            // 
+            this.b_Putere.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(143)))), ((int)(((byte)(112)))));
+            this.b_Putere.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_Putere.ForeColor = System.Drawing.Color.Honeydew;
+            this.b_Putere.Location = new System.Drawing.Point(254, 360);
+            this.b_Putere.Name = "b_Putere";
+            this.b_Putere.Size = new System.Drawing.Size(60, 60);
+            this.b_Putere.TabIndex = 18;
+            this.b_Putere.Text = "^";
+            this.b_Putere.UseVisualStyleBackColor = false;
+            this.b_Putere.Click += new System.EventHandler(this.b_Putere_Click);
+            // 
+            // b_Egal
+            // 
+            this.b_Egal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(122)))), ((int)(((byte)(102)))));
+            this.b_Egal.Font = new System.Drawing.Font("Verdana", 14F);
+            this.b_Egal.ForeColor = System.Drawing.Color.Honeydew;
+            this.b_Egal.Location = new System.Drawing.Point(103, 360);
+            this.b_Egal.Name = "b_Egal";
+            this.b_Egal.Size = new System.Drawing.Size(126, 60);
+            this.b_Egal.TabIndex = 19;
+            this.b_Egal.Text = "=";
+            this.b_Egal.UseVisualStyleBackColor = false;
+            this.b_Egal.Click += new System.EventHandler(this.b_Egal_Click);
+            // 
+            // b_Rad
+            // 
+            this.b_Rad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(143)))), ((int)(((byte)(112)))));
+            this.b_Rad.Font = new System.Drawing.Font("Verdana", 12F);
+            this.b_Rad.ForeColor = System.Drawing.Color.Honeydew;
+            this.b_Rad.Location = new System.Drawing.Point(169, 98);
+            this.b_Rad.Name = "b_Rad";
+            this.b_Rad.Size = new System.Drawing.Size(60, 60);
+            this.b_Rad.TabIndex = 20;
+            this.b_Rad.Text = "log";
+            this.b_Rad.UseVisualStyleBackColor = false;
+            this.b_Rad.Click += new System.EventHandler(this.b_Rad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(360, 487);
+            this.Controls.Add(this.b_Rad);
+            this.Controls.Add(this.b_Egal);
+            this.Controls.Add(this.b_Putere);
+            this.Controls.Add(this.b_Minus);
+            this.Controls.Add(this.b_Ori);
+            this.Controls.Add(this.b_Imp);
+            this.Controls.Add(this.b_Plus);
+            this.Controls.Add(this.b_Sterge);
+            this.Controls.Add(this.b_c);
+            this.Controls.Add(this.b_0);
+            this.Controls.Add(this.b_9);
+            this.Controls.Add(this.b_8);
+            this.Controls.Add(this.b_7);
+            this.Controls.Add(this.b_6);
+            this.Controls.Add(this.b_5);
+            this.Controls.Add(this.b_4);
+            this.Controls.Add(this.b_3);
+            this.Controls.Add(this.b_2);
+            this.Controls.Add(this.b_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULATOR";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button b_1;
+        private System.Windows.Forms.Button b_2;
+        private System.Windows.Forms.Button b_3;
+        private System.Windows.Forms.Button b_6;
+        private System.Windows.Forms.Button b_5;
+        private System.Windows.Forms.Button b_4;
+        private System.Windows.Forms.Button b_9;
+        private System.Windows.Forms.Button b_8;
+        private System.Windows.Forms.Button b_7;
+        private System.Windows.Forms.Button b_0;
+        private System.Windows.Forms.Button b_c;
+        private System.Windows.Forms.Button b_Sterge;
+        private System.Windows.Forms.Button b_Plus;
+        private System.Windows.Forms.Button b_Imp;
+        private System.Windows.Forms.Button b_Ori;
+        private System.Windows.Forms.Button b_Minus;
+        private System.Windows.Forms.Button b_Putere;
+        private System.Windows.Forms.Button b_Egal;
+        private System.Windows.Forms.Button b_Rad;
+    }
+}
+
---
+++ Projects C#/Calculator_Abrudan_Rebeca/Calculator_Abrudan_Rebeca/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace Calculator_Abrudan_Rebeca
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr Handle, int Msg, int Param1, int Param2);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int nr1, nr2, op; // +=1, -=2, /=3, *=4, ^=5, rad=6
+
+        private void b_3_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(3);
+        }
+
+        private void b_4_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(4);
+        }
+
+        private void b_5_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(5);
+        }
+
+        private void b_6_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(6);
+        }
+
+        private void b_7_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(7);
+        }
+
+        private void b_8_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(8);
+        }
+
+        private void b_9_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(9);
+        }
+
+        private void b_0_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(0);
+        }
+
+        private void b_Plus_Click(object sender, EventArgs e)
+        {
+            nr1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            op = 1;
+        }
+
+        private void b_Minus_Click(object sender, EventArgs e)
+        {
+            nr1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            op = 2;
+        }
+
+        private void b_Ori_Click(object sender, EventArgs e)
+        {
+            nr1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            op = 4;
+        }
+
+        private void b_Imp_Click(object sender, EventArgs e)
+        {
+            nr1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            op = 3;
+        }
+
+        private void b_Egal_Click(object sender, EventArgs e)
+        {
+            nr2 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            if (op == 1)
+            {
+                label1.Text = Convert.ToString(nr1 + nr2);
+            }
+            else
+            {
+                if (op == 2)
+                {
+                    label1.Text = Convert.ToString(nr1 - nr2);
+                }
+                else
+                {
+                    if (op == 3)
+                    {
+                        if (nr2 == 0)
+                            label1.Text = "Eroare!";
+                        else
+                        {
+                            float c;
+                            c = (float)nr1 / nr2;
+                            label1.Text = Convert.ToString(c);
+                        }
+                    }
+                    else
+                    {
+                        if (op == 4)
+                        {
+                            label1.Text = Convert.ToString(nr1 * nr2);
+                        }
+                        else
+                        {
+                            if (op == 5)
+                            {
+                                label1.Text = Convert.ToString(Math.Pow(nr1, nr2));
+                            }
+                            else
+                            {
+                                if(op==6)
+                                {
+                                    label1.Text = Convert.ToString(Math.Log(nr2, nr1));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void b_c_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void b_Sterge_Click(object sender, EventArgs e)
+        {
+            string s = label1.Text;
+            if (s.Length > 1)
+                s = s.Substring(0, s.Length - 1);
+            else
+                s = "";
+            label1.Text = s;
+            if (op == 0)
+            {
+                if (s != "")
+                    nr1 = Convert.ToInt32(label1.Text);
+                else
+                    nr1 = 0;
+            }
+            else
+            {
+                if (s != "")
+                    nr2 = Convert.ToInt32(label1.Text);
+                else
+                    nr2 = 0;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+
+        private void b_Putere_Click(object sender, EventArgs e)
+        {
+            nr1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            op = 5;
+        }
+
+        private void b_Rad_Click(object sender, EventArgs e)
+        {
+            nr1 = Convert.ToInt32(label1.Text);
+            label1.Text = "";
+            op = 6;
+        }
+
+        private void b_2_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(2);
+        }
+
+        private void b_1_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + Convert.ToString(1);
+        }
+    }
+}
